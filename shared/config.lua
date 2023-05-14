f73944f3d18a7cc51cbc89d6eec8eb03506f637c
Sarmingoo <78432708+Sarmingo@users.noreply.github.com>
2023-05-14 12:05:43 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,7 +1,6 @@
 Config = {}
 
 Config.OxInventory = false -- put true if you using ox inventory
-Config.NewLegacy = true -- put true if you using newest esx core or put false if you using old
 
 Config.Ped = {
     {
