c32079f8a49a9257ea2e5d6bed8c1745c3bf94d0
Sarmingoo <78432708+Sarmingo@users.noreply.github.com>
2023-05-14 11:11:17 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,13 @@
 if not Config.NewLegacy then
     ESX = nil
-Citizen.CreateThread(function()
-            while ESX == nil do
-                TriggerEvent('esx:getSharedObject', function(obj)
-                        ESX = obj
-                    end)
-                Citizen.Wait(0)
-            end
-        end)
+    Citizen.CreateThread(function()
+        while ESX == nil do
+            TriggerEvent('esx:getSharedObject', function(obj)
+                ESX = obj
+            end)
+            Citizen.Wait(0)
+        end
+    end)
 end
 
 Ped = {}
